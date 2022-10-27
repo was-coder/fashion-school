1de137dfe7dd13a9cbc240bcffa2024f89f56e28
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-10-27 05:30:48 +0100

    fix: remove the getRoute routes from import

--- src/imports/Import.js
+++ src/imports/Import.js
@@ -4,5 +4,4 @@ export { default as About } from "../components/About";
 export { default as AboutId } from "../components/AboutId";
 export { default as Contact } from "../components/Contact";
 export { default as ErrorPage } from "../errors/ErrorPage";
-export { default as GetRoutes } from "../routes/GetRoutes";
 export { default as ErrorBoundary } from "../errors/ErrorBoundary";
