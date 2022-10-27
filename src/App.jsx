5f442a74c36ac35f655a5d09cfed9583d1e0e919
Abdu Wasi' Adelakun <wasleadwears4u2c@gmail.com>
2022-10-27 07:11:57 +0100

    fix:resolve the GetRoutes path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { NavBar, GetRoutes, ErrorBoundary } from "./imports/Import";
+// import GetRoutes from "./routes/GetRoutes";
 
 function App() {
   return (
